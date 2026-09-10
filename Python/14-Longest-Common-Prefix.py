f56e3d4607b0a1c153b4aee70b220ffabbb1656e
prithi-18-rency <prithirency46@gmail.com>
2026-09-10 12:07:53 +0530

    [14] [Time Beats: 100.00%] [Memory Beats: 70.14%] - LeetPush

---
+++ Python/14-Longest-Common-Prefix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix = strs[0]
+        for word in strs:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+        return prefix
+        
