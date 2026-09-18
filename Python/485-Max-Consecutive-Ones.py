01098473af3f9038df3f290fb35b54cddd1c6b47
prithi-18-rency <prithirency46@gmail.com>
2026-09-18 08:37:20 +0530

    [485] [Time Beats: 5.39%] [Memory Beats: 9.69%] - LeetPush

--- Python/485-Max-Consecutive-Ones.py
+++ Python/485-Max-Consecutive-Ones.py
@@ -1,13 +1,7 @@
 class Solution(object):
     def findMaxConsecutiveOnes(self, nums):
-        maxs=0
-        count=0
+        maxs=count=0
         for i in range(len(nums)):
-            if nums[i]==1:
-                count+=1
-            else:
-                count=0    
-            if maxs<count:
-                maxs=count
-        return maxs            
-        
+            count=count+1 if nums[i]==1 else 0
+            maxs=max(count,maxs)
+        return maxs    
